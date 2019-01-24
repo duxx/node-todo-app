4e2ebe942e13d88db36cb15ec2121fd128260a7a
Antti Sand <antti.sand@gmail.com>
2019-01-24 14:31:02 +0200

    Mongo+express

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const {mongoose} = require('./db/mongoose')
+const {Todo} = require('./models/todo')
+const {User} = require('./models/user')
+
+let app = express()
+
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    })
+
+    todo.save().then((doc) => {
+        res.send(doc)
+    }, (e) => {
+        console.log("Error")
+        res.status(400).send()
+    })
+})
+
+
+
+app.listen(3000, () => {
+    console.log("Server listening on port 3000")
+})
