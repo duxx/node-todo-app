aa899f6f3720d4e2ac32ce035c140f2ddf6ac0b0
Antti Sand <antti.sand@gmail.com>
2019-01-28 16:01:00 +0200

    Add login route

--- server/models/user.js
+++ server/models/user.js
@@ -68,11 +68,31 @@ UserSchema.statics.findByToken = function(token) {
     })
 }
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    let User = this
+
+    return User.findOne({email}).then((user) => {
+        if(!user) {
+            return Promise.reject()
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res) {
+                    resolve(user)
+                } else {
+                    reject()
+                }
+            })
+        })
+    })
+}
+
 UserSchema.pre('save', function(next) {
     let user = this
 
     if(user.isModified('password')) {
-        bcrypt.getSalt(10, (err, salt) => {
+        bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(user.password, salt, (err, hash) => {
                 user.password = hash;
                 next()
